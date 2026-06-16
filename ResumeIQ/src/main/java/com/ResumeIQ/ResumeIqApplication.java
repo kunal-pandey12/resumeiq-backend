9bda0f1ee8fd1a25d842ee746de846b54e9bfcc3
kunal-pandey12 <kunalpandey.12dev@gmail.com>
2026-06-16 23:42:00 +0530

    Initial commit - ResumeIQ backend

--- ResumeIQ/src/main/java/com/ResumeIQ/ResumeIqApplication.java
+++ ResumeIQ/src/main/java/com/ResumeIQ/ResumeIqApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ResumeIqApplication {
 
 	public static void main(String[] args) {
-
         SpringApplication.run(ResumeIqApplication.class, args);
 	}
-
 }
